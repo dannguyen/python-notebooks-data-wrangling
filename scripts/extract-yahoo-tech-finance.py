ea7d3761f0c28589942f204123bcb8dd7082117b
Dan Nguyen <dansonguyen@gmail.com>
2016-05-03 23:52:37 -0700

    more finance

--- scripts/extract-yahoo-tech-finance.py
+++ scripts/extract-yahoo-tech-finance.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import requests
-BASE_URL='http://real-chart.finance.yahoo.com/table.csv?s={ticker_symbol}&a=00&b=1&c=1900&d=04&e=1&f=2016&g=d&ignore=.csv'
-TICKERS = ['FB', 'YHOO', 'MSFT', 'GOOG', 'YELP', 'DATA', 'LNKD', 'BOX', 'CRM',
-           'TWTR', 'SQ', 'AMZN', 'GRPN', 'ZNGA', 'P', 'EBAY', 'PYPL', 'NFLX']
-DATA_DIR = Path('data', 'financial', 'raw')
-DATA_DIR.mkdir(exist_ok=True)
+BASE_URL='http://real-chart.finance.yahoo.com/table.csv?s={ticker_symbol}&a=00&b=1&c=1900&d=04&e=30&f=2016&g=d&ignore=.csv'
+TICKERS = ['AAPL', 'AMZN', 'BABA', 'BIDU', 'BOX', 'CRM', 'CSCO', 'DATA',
+           'EBAY', 'EXPE', 'FB', 'FIT', 'GOOG', 'GPRO', 'GRPN', 'GRUB',
+           'HPQ', 'HUBS', 'IBM', 'LNKD', 'MSFT', 'NFLX', 'ORCL', 'P', 'PCLN',
+           'PYPL', 'RENN', 'SQ', 'TWTR', 'YELP', 'YHOO', 'YNDX', 'ZEN', 'ZNGA']
 
+DATA_DIR = Path('data', 'financial', 'raw', 'companies')
+DATA_DIR.mkdir(exist_ok=True)
 
 for t in TICKERS:
     print("Downloading ticker:", t)
